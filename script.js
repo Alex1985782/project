3333172c9c966059a816ad7fde29430e80e4b982
Alex <>
2020-04-03 23:12:15 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var bob = 2;
+console.log(bob);
+
+//alert("Hello World");
+
+/* let answer = confirm("Are you here?");
+  console.log(answer); */
+
+  /*let answer = prompt("ты охуел?", "иди на хуй");
+  console.log(typeof(null));*/
+  console.log("2" == 2);

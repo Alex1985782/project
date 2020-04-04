033e80dbe4118e289235f1c32a6452eb7ec7a736
Alex <korobka1985@yandex.ru>
2020-04-04 22:10:41 +0300

    quest

--- script.js
+++ script.js
@@ -1,13 +1,26 @@
 "use strict";
 
-var bob = 2;
-console.log(bob);
+  // var number = 5; (все числа в том числе и дробные, а также слова infinity (4/0) и NaN ("Раздел"/2))
+  // var string = "Hello!";
+  // var sym = Symbol();
+  // var boolean = true;
+  // null; (ссылка на не существующий элемент)
+  // undefined; (к примеру когда переменную не обозначили)
+  // var obj = {}; (комплекс данных)
 
-//alert("Hello World");
+  /*let arr ["","",]; - это массив, он содержит любые типы данных и оборачивается в [] скобки)
+  console.log(arr[2]); выведет значение под номером два, нумерация стартует с нуля.*/
+  
+  /*let persone = {
+      name: "Jack",
+      age: 37,
+      isMarried: true
+  };
+  console.log(persone.name);*/
 
-/* let answer = confirm("Are you here?");
-  console.log(answer); */
+  
+ let money = confirm("Ваш бюджет на месяц?");
+  console.log(money);
 
-  /*let answer = prompt("ты охуел?", "иди на хуй");
-  console.log(typeof(null));*/
-  console.log("2" == 2);
+let time = prompt("Введите дату в формате YYYY-MM-DD", "");
+ console.log(time);

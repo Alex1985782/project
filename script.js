de599f234bfd73db2085fdbd4ff45c5bc4d64cd2
Alex <korobka1985@yandex.ru>
2020-04-05 23:28:35 +0300

    task

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-"use strict";
+"use strict"; // строгий режим, так как мы работаем на новом стандарте EC6, то старые ошибки не работают, а
+//эта приблуда их замечает.
 
   // var number = 5; (все числа в том числе и дробные, а также слова infinity (4/0) и NaN ("Раздел"/2))
   // var string = "Hello!";
@@ -10,17 +11,63 @@
 
   /*let arr ["","",]; - это массив, он содержит любые типы данных и оборачивается в [] скобки)
   console.log(arr[2]); выведет значение под номером два, нумерация стартует с нуля.*/
-  
-  /*let persone = {
-      name: "Jack",
-      age: 37,
-      isMarried: true
-  };
-  console.log(persone.name);*/
+ 
+  /* let answer = +prompt("Есть ли вам 18?", "Да");
+     console.log(typeof(answer)); выводит название типа данных*/
+
+//   let persone = {
+//       name: "Jack",
+//       age: 37,
+//       isMarried: true
+//   };
+//   console.log(persone.name);
 
   
- let money = confirm("Ваш бюджет на месяц?");
-  console.log(money);
+/*let plus = 10,
+    minus = 10;
 
-let time = prompt("Введите дату в формате YYYY-MM-DD", "");
- console.log(time);
+    plus++;
+    minus--;
+    console.log(minus, plus);*/
+    
+    // let bober = true,
+    //     rat = true;
+
+    //  console.log(bober && rat);   
+
+
+let money = prompt("Ваш бюджет на месяц?");
+  //console.log(money);
+
+let time = prompt("Введите дату в формате", "YYYY-MM-DD");
+  //console.log(time);
+
+let appData = {
+
+  бюджет: money,
+  timeData: time,
+  
+  a: prompt("Введите обязательную статью расходов в этом месяце"),
+  b: prompt("Во сколько обойдется?"),
+  c: prompt("Введите обязательную статью расходов в этом месяце"),
+  d: prompt("Во сколько обойдется?"),
+     
+  expenses : {}, 
+  expenses.b = c,
+  expenses = {
+    b: "c"
+  },
+
+  optionalExpenses : {},
+
+  income : [],
+  //console.log(arr[2]); выведет значение под номером два, нумерация стартует с нуля.*/
+ 
+  savings: false
+};
+
+console.log(appData.бюджет, appData.timeData, appData.expenses);
+
+
+
+

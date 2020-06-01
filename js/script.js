196af035e34bf66102bdf66366d5b34c75f5a47c
Alex <korobka1985@yandex.ru>
2020-06-01 10:44:24 +0300

    Task1

---
+++ js/script.js
@@ -0,0 +1,96 @@
+"use strict"; // строгий режим, так как мы работаем на новом стандарте EC6, то старые ошибки не работают, а
+//эта приблуда их замечает.
+
+let money, time;
+ 
+function start() {
+  money = +prompt("Ваш бюджет на месяц?", '');
+  time = prompt('Введите дату в формате', 'YYYY-MM-DD');
+  while(isNaN(money) || money == "" || money == null){ /* команда isNan возвращает true, 
+    когда в содержимое попадают не цифры, а буквы и т.д. => суть в том, что цикл while(пока) - 
+    будет повторяться до тех пор, пока пользователь не введет цифры.
+    Поэтому далее мы через || (или) прописываем еще "" - пустые строки и null - кнопка отмены.*/
+    money = +prompt("Ваш бюджет на месяц?", '');
+  }
+}
+  start();
+
+  let appData = {
+      budget: money,
+      expenses: {},
+      optionalExpenses: {},
+      income: [],
+      timeData: time,
+      savings: true
+  };
+  
+function chooseExpenses() {
+  for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+        b = prompt("Во сколько обойдется?", '');
+    
+    if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null && 
+    a != "" && b != "" && a.length < 50) {
+          console.log("done");
+          appData.expenses[a] = b; 
+           
+        } else {
+         console.log('Ответьте пожалуйста на вопросы');
+        i--;
+        }          
+    }       
+}  
+chooseExpenses();
+
+ 
+
+  function detectDayBudget() {
+    appData.moneyPerDay = (appData.budget / 30).toFixed(); /*.toFixed() округляет число, если в скобках 
+    поставить 1, то после запятой добавится еще десятая - 1,3. Также это команда превращает в строковое число*/      
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);  
+  }
+  
+  detectDayBudget();
+  
+
+  function detectLevel() {
+    if(appData.moneyPerDay < 100) {
+      console.log("Минимальный уровень достатка");
+  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+      console.log("Средний уровень достатка");
+  } else if (appData.moneyPerDay >= 2000) {
+    console.log("Высокий уровень достатка");
+  } else {
+    console.log("Произошла ошибка");
+  }
+  }
+  detectLevel();
+
+  function checkSavings() {
+    if(appData.savings == true) {
+      let save = +prompt("Какова сумма накоплений?"),
+          percent = +prompt("Под какой процент?");
+
+    appData.monthIncome = save/100/12*percent;
+    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+  }
+  checkSavings();  
+  
+  
+  function chooseOptExpenses() {
+       for(let q = 0; q < 3; q++) {
+        optionalExpenses.quest = prompt("Статья необязательных расходов?");         
+       console.log(optionalExpenses.quest);
+       
+       }
+       
+    }    
+    let optionalExpenses = {
+      quest: {}
+    };
+    chooseOptExpenses();
+    
+    
+
+  
--- script.js
+++ script.js
@@ -1,100 +1,53 @@
-"use strict"; // строгий режим, так как мы работаем на новом стандарте EC6, то старые ошибки не работают, а
-//эта приблуда их замечает.
+"use strict"; 
 
-let money, time;
- 
-function start() {
-  money = +prompt("Ваш бюджет на месяц?", '');
-  time = prompt('Введите дату в формате', 'YYYY-MM-DD');
-  while(isNaN(money) || money == "" || money == null){ /* команда isNan возвращает true, 
-    когда в содержимое попадают не цифры, а буквы и т.д. => суть в том, что цикл while(пока) - 
-    будет повторяться до тех пор, пока пользователь не введет цифры.
-    Поэтому далее мы через || (или) прописываем еще "" - пустые строки и null - кнопка отмены.*/
-    money = +prompt("Ваш бюджет на месяц?", '');
-  }
-}
-  start();
+                                  // Объект
 
-  let appData = {
-      budget: money,
-      expenses: {},
-      optionalExpenses: {},
-      income: [],
-      timeData: time,
-      savings: true
-  };
-  
-function chooseExpenses() {
-  for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-        b = prompt("Во сколько обойдется?", '');
-    
-    if ((typeof(a)) === "string" && (typeof(a)) != null && (typeof(b)) != null && 
-    a != "" && b != "" && a.length < 50) {
-          console.log("done");
-          appData.expenses[a] = b; 
-           
-        } else {
-         console.log('Ответьте пожалуйста на вопросы');
-        i--;
-        }          
-    }       
-}  
-chooseExpenses();
+// const obj = {
+//     name: "Alex",
+//     age: 35,
+//     isMarried: true
+// };
+// console.log(obj.age);
 
- 
 
-  function detectDayBudget() {
-    appData.moneyPerDay = (appData.budget / 30).toFixed(); /*.toFixed() округляет число, если в скобках 
-  поставить 1, то после запятой добавится еще десятая - 1,3. Также это команда превращает в строковое число*/
-  
-  let everyDaybudget = ("Ежедневный бюджет: " + appData.moneyPerDay);
-  return everyDaybudget;
-  }
-  
-  let z = detectDayBudget();
-  detectDayBudget(z);
+                                 // Объект массив
 
-  function detectLevel() {
-    if(appData.moneyPerDay < 100) {
-      console.log("Минимальный уровень достатка");
-  } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-      console.log("Средний уровень достатка");
-  } else if (appData.moneyPerDay >= 2000) {
-    console.log("Высокий уровень достатка");
-  } else {
-    console.log("Произошла ошибка");
-  }
-  }
-  detectLevel();
+// let arr = ["plum.png", "балбес", "47", {}, []];
+// console.log(arr[0]);
 
-  function checkSavings() {
-    if(appData.savings == true) {
-      let save = +prompt("Какова сумма накоплений?"),
-          percent = +prompt("Под какой процент?");
 
-    appData.monthIncome = save/100/12*percent;
-    alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
-    }
-  }
-  checkSavings();
-  
-  let optionalExpenses = {
-     
-  };
-  
-    //       name: "Jack",
-    //       age: 37,
-    //       isMarried: true
-    //   };
-    //   console.log(persone.name);
-  
-  function chooseOptExpenses() {
-    for(let q = 0; q < 3; q++) {
-      let quest = prompt("Статья необязательных расходов?");
-      
-    }    
-  }
-  chooseOptExpenses();
+                                // Обратная связь с пользователем
 
-  
+// let answer = confirm('Вам есть 18?', '');
+// console.log(answer);
+
+// let question = +prompt('Сколько вам лет?', '18');
+// console.log(question);
+
+
+                                // Запись в массив
+
+// const arrs = [];
+// arrs[0] = prompt('Как тебя зовут?', '');
+// arrs[1] = prompt('Какая у тебя фамилия?', '');
+// arrs[2] = prompt('Сколько тебе лет?', '');
+// console.log(arrs);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMoveDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a  = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c  = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMoveDB.movies[a] = b;
+      personalMoveDB.movies[c] = d;
+      console.log(personalMoveDB);

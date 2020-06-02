89cba2387977c5707afdc60cd30b654bb1c297d6
Alex <korobka1985@yandex.ru>
2020-06-02 10:31:58 +0300

    Task2

--- script.js
+++ script.js
@@ -33,8 +33,89 @@
 // arrs[2] = prompt('Сколько тебе лет?', '');
 // console.log(arrs);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+// const personalMoveDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
+
+//       personalMoveDB.movies[a] = b;
+//       personalMoveDB.movies[c] = d;
+//       console.log(personalMoveDB);
+
+//       let num = 50;
+
+//       if (num < 49) {
+//           console.log('Error');
+//       } else if (num > 100) {
+//           console.log('no');
+//       } else {
+//           console.log('ok');
+//       }
+
+
+//       // Тернарный оператор
+
+//       (num == 50) ? console.log('ok') :  console.log('no'); 
+
+
+      //Строгое сравнение и только. Можно сравнивать строки
+
+    //   let num = 50;
+
+    //   switch (num) {    
+    //       case 49:
+    //           console.log('Неверно');
+    //           break;
+    //       case 100:
+    //           console.log('Неверно');
+    //           break;
+    //       case 50:
+    //           console.log('В точку!');
+    //           break;
+    //       default:
+    //           console.log('Не в этот раз');
+    //   }
+
+    //   let num = 50;
+
+    //   while (num <= 60) {
+    //       console.log(num);
+    //       num++;
+    //   }
+    // do {
+    //     console.log(num);
+    //     num++;
+    // } while (num <= 55);
+
+    // for (let i = 0; i < 7; i++ ) {  
+    //Если условие будет 0, то есть i < 0, то цикл будет выполнятся вечно, 
+    //поскольку значение i каждый раз будет увеличиваться на 1
+    //     console.log(num);
+    //     num++;
+        
+    // }
+
+    // for ( let i = 1; i < 7; i++) {
+    //     if (i === 3) {
+    //         // break; //прерывает итерацию по достижению значения = 3
+    //         continue; //в отличие от команды break - кманда continue продолжает итерацию, но не выводит значеиие 3
+    //     }
+    //     console.log(i);
+    // }
+   
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    
 const personalMoveDB = {
     count: numberOfFilms,
     movies: {},
@@ -43,11 +124,61 @@ const personalMoveDB = {
     privat: false
 };
 
-const a  = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c  = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+if (personalMoveDB.count <= 10) {
+  console.log('Просмотрено довольно мало фильмов');
+} else if (personalMoveDB.count <= 30) {
+  console.log('Вы классический зритель');
+} else if (personalMoveDB.count > 30) {
+  console.log('Вы киноман');
+} else {
+  console.log('Произошла ошибка');
+}
 
-      personalMoveDB.movies[a] = b;
-      personalMoveDB.movies[c] = d;
-      console.log(personalMoveDB);
+// for (let i = 1; i < 3; i++) {
+//     let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//         personalMoveDB.movies[a] = b;
+//           if (a == '' || a == null || a.length > 5 || b == '' || b == null || a.length > 50) {           
+//            i--;           
+//           }
+          
+// }
+// let i = 1;
+// while (i < 3) {
+//  let a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//         personalMoveDB.movies[a] = b;
+//           if (a == '' || a == null || a.length > 5 || b == '' || b == null || a.length > 50) {           
+//            i--;           
+//           }
+
+// }
+
+// let i = 1;
+// do {
+//   let a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', '');
+//           personalMoveDB.movies[a] = b;
+//           i++;
+//             if (a == '' || a == null || a.length > 5 || b == '' || b == null || a.length > 50) {           
+//              i--;           
+//             }
+//           } while (i < 3);
+           
+//       console.log(personalMoveDB);
+
+
+
+// Препод
+// for (let i = 0; i < 2; i++) {
+//       const a = prompt('Один из последних просмотренных фильмов?', ''),
+//             b = prompt('На сколько оцените его?', '');
+         
+//             if (a != '' && b != '' && a != null && b != null && a.length < 50) {  
+//              personalMoveDB.movies[a] = b;         
+//              console.log('done');           
+//             } else {
+//               i--;
+//               console.log('error');
+//             }
+//           }
